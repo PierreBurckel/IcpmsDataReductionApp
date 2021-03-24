c5b9a3caea2c2220bcef747d066899bd4c20db31
Pierre Burckel <pierreburckel@gmail.com>
2021-03-24 08:06:28 +0100

    adding the R6 file for oo code

---
+++ ICPMS_R6.R
@@ -0,0 +1,68 @@
+library(shiny)
+
+reactiveTrigger <- function() {
+  counter <- reactiveVal(0)
+  list(
+    depend = function() {
+      counter()
+      invisible()
+    },
+    trigger = function() {
+      counter( isolate(counter()) + 1 )
+    }
+  )
+}
+
+IcpDataReactive <- R6::R6Class(
+  public = list(
+    initialize = function(IcpData) {
+      private$countValues = IcpData$countValues
+      private$countSd = IcpData$countSd
+    },
+    getCountValues = function() {
+      private$rxTrigger$depend()
+      return(private$countValues)
+    },
+    getCountSd = function() {
+      private$rxTrigger$depend()
+      return(private$countSd)
+    },
+    getCountRsd = function() {
+      private$rxTrigger$depend()
+      countRsd <- private$countSd / private$countValues * 100
+      return(countRsd)
+    },
+    getElementFullNames = function() {
+      private$rxTrigger$depend()
+      return( colnames(private$countValues) )
+    },
+    getMetadata = function() {
+      private$rxTrigger$depend()
+      return(private$metadata)
+    },
+    getMetadataNames = function() {
+      private$rxTrigger$depend()
+      return( colnames(private$metadata) ) 
+    },
+    setCountValuesAndSd = function(IcpData) {
+      private$rxTrigger$trigger()
+      private$countValues = IcpData$countValues
+      private$countSd = IcpData$countSd
+    },
+    setCountValuesAndRsd = function(IcpData) {
+      private$rxTrigger$trigger()
+      private$countValues = IcpData$countValues
+      private$countSd = IcpData$countRsd / 100 * IcpData$countValues
+    },
+    setMetadata = function(IcpMetadata) {
+      private$rxTrigger$trigger()
+      private$metadata = IcpMetadata
+    }
+  ),
+  private = list(
+    countValues = NULL,
+    countSd = NULL,
+    metadata = NULL,
+    rxTrigger = reactiveTrigger()
+  )
+)
